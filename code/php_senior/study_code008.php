7c9257e70c402329067ae9d116097c124c95a5bd
kevin <chantaldzl@sina.com>
2018-06-09 10:05:39 +0800

    first commit study_code008 php file

---
+++ code/php_senior/study_code008.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>用户登录</title>
+</head>
+<body>
+	<form action="study_code008_001.php" method="post">
+		<label>用户名：<input type="text" name="user_name" id=""></label><br>
+		<label>密　码：<input type="password" name="user_password" id=""></label><br>
+		<input type="submit" value="提交">
+	</form>
+</body>
+</html>
